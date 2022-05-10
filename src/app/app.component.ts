0d173e3227873fc88366dda8fc74d48ac0de0c6e
GonzaVega <gonza.vega@icloud.com>
2022-05-10 13:32:58 -0300

    app upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'FrontEnd-Challenge';
+  // isLoggedIn = true;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+  // onLogIn() {
+  //   this.isLoggedIn = false;
+  // }
+  // onLogOut() {
+  //   this.isLoggedIn = true;
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ConferencesComponent } from './conferences/conferences.component';
+import { AboutComponent } from './about/about.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  // { path: '', component: HeaderComponent },
+  {
+    path: 'Conferences',
+    component: ConferencesComponent,
+  },
+
+  {
+    path: 'About',
+    component: AboutComponent,
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ConferencesComponent,
+    AboutComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
